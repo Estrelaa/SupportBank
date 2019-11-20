e3964419440205749dfbffc37626a56c2566a9ed
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-20 13:27:16 +0000

    The program can now read the from colum, or any colum. Hardcoded right now, fixing that

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SupportBank
 {
@@ -6,7 +9,27 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variables needed
+            string[] contentsOfFile = File.ReadAllLines("C:\\Work\\Training\\SupportBank\\Transactions2014.csv");
+            string Record = string.Join(",", contentsOfFile);
+            List<string> contentofFileSplit = new List<string>();
+
+
+            //parse data into account names
+            contentofFileSplit = Record.Split(",").ToList();
+
+            for (int i = 1; i < contentofFileSplit.Count; i++)
+            {
+                if (i % 5 == 1)
+                {
+                    Console.WriteLine(contentofFileSplit[i]);
+                }
+
+            }
+
+            //accountName.ForEach(Console.WriteLine);
+            Console.ReadLine();
+
         }
     }
 }
