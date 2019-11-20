05f93a157b7092e233a8fe01b57feb3746e5a703
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-20 13:39:25 +0000

    Seperated the parsing and save of the records to its own method

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -10,26 +10,36 @@ namespace SupportBank
         static void Main(string[] args)
         {
             //Variables needed
-            string[] contentsOfFile = File.ReadAllLines("C:\\Work\\Training\\SupportBank\\Transactions2014.csv");
-            string Record = string.Join(",", contentsOfFile);
             List<string> contentofFileSplit = new List<string>();
+            List<string> Record = new List<string>();
+
+            //Open the .csv file, save it as a array then join it into 1 long string to get rid of all the \n's
+            string[] contentsOfFile = File.ReadAllLines("C:\\Work\\Training\\SupportBank\\Transactions2014.csv");
+            string convertContentsToOneString = string.Join(",", contentsOfFile);
+
+            //Split the long string so every value in the .csv is its own element
+            contentofFileSplit = convertContentsToOneString.Split(",").ToList();
 
 
-            //parse data into account names
-            contentofFileSplit = Record.Split(",").ToList();
+            parseContentForValues(contentofFileSplit, 1, Record);
 
+            //accountName.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+
+        }
+
+        private static void parseContentForValues(List<string> contentofFileSplit, int ColumNumber,
+            List<string> variableToHoldRecords)
+        {
             for (int i = 1; i < contentofFileSplit.Count; i++)
             {
-                if (i % 5 == 1)
+                if (i % 5 == ColumNumber)
                 {
-                    Console.WriteLine(contentofFileSplit[i]);
+                    variableToHoldRecords.Add(contentofFileSplit[i]);
                 }
 
             }
-
-            //accountName.ForEach(Console.WriteLine);
-            Console.ReadLine();
-
         }
     }
 }
