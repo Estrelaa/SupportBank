a2769865dd3c4f808fd03e7fc208b0b0029e853a
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-22 10:20:44 +0000

    Updated comments and removed blank lines to shorten the file

--- SupportBank/ReadFiles.cs
+++ SupportBank/ReadFiles.cs
@@ -9,18 +9,16 @@ namespace SupportBank
     {
         public static void ReadCSV(out List<string> contentofFileWhenSplit, out string[] contentsOfFile)
         {
-            //Open the .csv file, save it as a array then join it into 1 long string to get rid of all the \n's
+            //Open the .csv files, save it as a list to add both files in, then join it into 1 long string to get rid of all the \n's
             List<string> contentsOfFiletemp = new List<string>();
 
             contentsOfFiletemp = File.ReadAllLines("C:\\Work\\Training\\SupportBank\\Transactions2014.csv").ToList();
-
-
             contentsOfFiletemp.AddRange(File.ReadAllLines("C:\\Work\\Training\\SupportBank\\DodgyTransactions2015.csv").ToList());
             contentsOfFile = contentsOfFiletemp.ToArray();
 
             string convertContentsToOneString = string.Join(",", contentsOfFile);
 
-            //Split the long string so every value in the .csv is its own element
+            //Split the long string so every value in the .csv is its own element, needed for the CreateAccounts method in parseAccounts.cs
             contentofFileWhenSplit = convertContentsToOneString.Split(",").ToList();
         }
     }
