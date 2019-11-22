271c78ff7654969c6e07135da83454cd9148b0a4
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-22 13:50:47 +0000

    Now Logs to the locations of all the files in the project

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -24,7 +24,7 @@ namespace SupportBank
         private static void LoggingInit()
         {
             var config = new LoggingConfiguration();
-            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            var target = new FileTarget { FileName = @"C:\Work\Training\SupportBank\SupportBank", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
             config.AddTarget("File Logger", target);
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
             LogManager.Configuration = config;
