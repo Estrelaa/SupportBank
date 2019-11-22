90c409cfd9c3c04b28b40bad0dc48d62ae57f187
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-22 12:01:14 +0000

    Fixed FindAccountTransactions bug where the total balance would be a negative when not in debt, added error checking for wrong values in a .CSV

--- SupportBank/CreateTransactions.cs
+++ SupportBank/CreateTransactions.cs
@@ -15,16 +15,30 @@ namespace SupportBank
                 if (Regex.IsMatch(Line, @"" + accountNameTofind + ""))
                 {
                     match = Line.Split(",");
-
+                    //TODO: Add try/catch to deal with "One Cheeseburger" in the amount colum becausing of ******* Ben
                     if (match[1] == accountNameTofind)
                     {
-                        Console.WriteLine("{0} need to pay £{1} to {2} because of {3} on {4} ", match[1], Math.Round(float.Parse(match[4]), 2), match[2], match[3], match[0]);
-                        AccountTotal = AccountTotal - float.Parse(match[4]);
+                        try
+                        {
+                            Console.WriteLine("{0} need to pay £{1} to {2} because of {3} on {4} ", match[1], Math.Round(float.Parse(match[4]), 2), match[2], match[3], match[0]);
+                            AccountTotal = AccountTotal - float.Parse(match[4]);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("A Value in this transaction is in a invaild format, skipping...");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("{0} Should be paid £{1} from {2} because of {3} on {4} ", match[2], Math.Round(float.Parse(match[4]), 2), match[1], match[3], match[0]);
-                        AccountTotal = AccountTotal + float.Parse(match[4]);
+                        try
+                        {
+                            Console.WriteLine("{0} Should be paid £{1} from {2} because of {3} on {4} ", match[2], Math.Round(float.Parse(match[4]), 2), match[1], match[3], match[0]);
+                            AccountTotal = AccountTotal + float.Parse(match[4]);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("A Value in this transaction is in a invaild format, skipping...");
+                        }
                     }
                 }
             }
